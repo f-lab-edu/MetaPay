046bd0d4f814eaedc29acf026018481608918d37
ydmins <dr.ydmins@gmail.com>
2024-07-17 22:20:35 +0900

    Init Product service

---
+++ payment-service/src/test/java/com/ydmins/metapay/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.metapay.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
