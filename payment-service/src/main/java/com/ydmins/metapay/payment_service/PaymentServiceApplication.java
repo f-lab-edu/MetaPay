bf3a605d9ecbade4acd7dd1ac7fdbee646f95f2c
ydmins <dr.ydmins@gmail.com>
2024-07-18 15:47:40 +0900

    feature: Add time auditing with TimeBaseEntity

--- payment-service/src/main/java/com/ydmins/metapay/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/ydmins/metapay/payment_service/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.ydmins.metapay.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
