81cac2d9597bf3b837aac17fed09184e18bce547
ydmins <dr.ydmins@gmail.com>
2024-08-18 17:58:57 +0900

    add: nGrinder 테스트 스크립트 생성

---
+++ performance-test/scipt/postPaymentsDynamicUserId.groovy
@@ -0,0 +1,73 @@
+import static net.grinder.script.Grinder.grinder
+import static org.junit.Assert.*
+import static org.hamcrest.Matchers.*
+import net.grinder.script.GTest
+import net.grinder.script.Grinder
+import net.grinder.scriptengine.groovy.junit.GrinderRunner
+import net.grinder.scriptengine.groovy.junit.annotation.BeforeProcess
+import net.grinder.scriptengine.groovy.junit.annotation.BeforeThread
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.ngrinder.http.HTTPRequest
+import org.ngrinder.http.HTTPRequestControl
+import org.ngrinder.http.HTTPResponse
+import org.ngrinder.http.cookie.Cookie
+import org.ngrinder.http.cookie.CookieManager
+import groovy.json.JsonOutput
+
+@RunWith(GrinderRunner)
+class TestRunner {
+
+    public static GTest test
+    public static HTTPRequest request
+    public static Map<String, String> headers = [:]
+    public static Map<String, Object> params = [:]
+    public static List<Cookie> cookies = []
+
+    @BeforeProcess
+    public static void beforeProcess() {
+        HTTPRequestControl.setConnectionTimeout(300000)
+        test = new GTest(1, "PaymentService Test")
+        request = new HTTPRequest()
+        grinder.logger.info("before process.")
+    }
+
+    @BeforeThread
+    public void beforeThread() {
+        test.record(this, "test")
+        grinder.statistics.delayReports = true
+        grinder.logger.info("before thread.")
+    }
+
+    @Before
+    public void before() {
+        request.setHeaders(["Content-Type": "application/json"])
+        CookieManager.addCookies(cookies)
+        grinder.logger.info("before. init headers and cookies")
+    }
+
+    @Test
+    public void test() {
+        def paymentRequest = [
+                amount: 105.00,
+                method: "CREDIT_CARD",
+                userId: "user${System.currentTimeMillis()}",
+                orderId: "order${System.currentTimeMillis()}"
+        ]
+
+        def jsonPayload = JsonOutput.toJson(paymentRequest)
+        byte[] postData = jsonPayload.getBytes("UTF-8")
+
+        HTTPResponse response = request.POST("http://54.180.229.94:8080/payments", postData, [])
+
+        if (response.statusCode == 301 || response.statusCode == 302) {
+            grinder.logger.warn("Warning. The response may not be correct. The response code was {}.", response.statusCode)
+        } else {
+            assertThat(response.statusCode, is(200))
+            // 'getBodyText()'를 사용하여 응답 본문을 가져옵니다.
+            assertTrue(Boolean.parseBoolean(response.getBodyText()))
+        }
+    }
+}
